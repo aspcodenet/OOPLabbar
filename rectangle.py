85de517878fe3e796d25b45032fbb3d582718415
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-10-05 14:19:27 +0200

    labs

--- rectangle.py
+++ rectangle.py
@@ -1,3 +1,19 @@
+class Person:
+    def __init__(self):
+        self._namn = ""
+    def setName(self,namn):
+        if len(namn) > 100:
+            return False 
+        self._namn = namn
+        return True
+
+stefan = Person()        
+fru = Person()        
+
+stefan.moveInWith(fru)
+
+
+
 #Steg   1 skapa en klass
 #       2 def __init__ ska ta self 
 #       3 i def __init__ skapa properties med _ prefix
